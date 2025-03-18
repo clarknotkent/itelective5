589f38ba367eff2232d5e40050757af40c1e1179
Kent Elrond Andionne L. Aspa <151520614+clarknotkent@users.noreply.github.com>
2025-03-18 08:32:36 +0800

    Updated to follow instructions of activity

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,13 +5,29 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Login'),
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => DashboardScreen()));
-          },
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Aspa Activity 7',
+            style: TextStyle(
+              fontSize: 28, 
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            child: Text('Login'),
+            onPressed: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(builder: (context) => DashboardScreen()),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
